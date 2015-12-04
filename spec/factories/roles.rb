a38bf71bd1ef1c4e5054dec8478c13121c056174
Travis Vignon <travis@lookitsatravis.com>
2015-12-04 12:17:35 -0600

    Added sequence to Role Factory name
    This is because it sometimes causes tests to fail by being non-unique

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
-  factory :role, class: ApiGuardian::Role do
-    name { Faker::Lorem.word }
-    default false
+  factory :role, class: ApiGuardian::Role do |f|
+    f.sequence(:name) { |n| Faker::Lorem.word + "#{n}" }
+    f.default false
 
     factory :default_role do
       default true
