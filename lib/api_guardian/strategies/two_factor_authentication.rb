c3ff531fe02bc3429e75749aa86a229a770c1261
Travis Vignon <travis@lookitsatravis.com>
2015-12-07 14:06:12 -0600

    Added 2FA auth strategy

---
+++ lib/api_guardian/strategies/two_factor_authentication.rb
@@ -0,0 +1,23 @@
+module ApiGuardian
+  module Strategies
+    class TwoFactorAuthentication
+      def self.authenticate_request(user, request)
+        return true unless ApiGuardian.configuration.enable_2fa
+
+        if user.otp_enabled
+          otp_header_name = ApiGuardian.configuration.otp_header_name
+          otp_code = request.headers[otp_header_name]
+
+          if !otp_code || otp_code.blank?
+            ApiGuardian::Jobs::SendOtp.perform_later user
+            fail ApiGuardian::Errors::TwoFactorRequired
+          end
+
+          return user.authenticate_otp otp_code, drift: 30
+        end
+
+        true
+      end
+    end
+  end
+end
---
+++ spec/strategies/two_factor_authentication_spec.rb
@@ -0,0 +1,71 @@
+require 'faker'
+
+describe ApiGuardian::Strategies::TwoFactorAuthentication do
+  # Methods
+  describe 'methods' do
+    let(:user) { mock_model(ApiGuardian::User) }
+    let(:mock_request) { instance_double(ActionDispatch::Request) }
+
+    describe '.authenticate_request' do
+      it 'returns true if 2FA is disabled' do
+        expect(ApiGuardian.configuration).to receive(:enable_2fa).and_return(false)
+
+        result = ApiGuardian::Strategies::TwoFactorAuthentication.authenticate_request(user, mock_request)
+
+        expect(result).to be true
+      end
+
+      it 'returns true if user\'s does not have OTP enabled' do
+        expect(ApiGuardian.configuration).to receive(:enable_2fa).and_return(true)
+        expect(user).to receive(:otp_enabled).and_return false
+
+        result = ApiGuardian::Strategies::TwoFactorAuthentication.authenticate_request(user, mock_request)
+
+        expect(result).to be true
+      end
+
+      it 'queues SendOtp and fails if OTP header value is missing' do
+        mock_headers = instance_double(ActionDispatch::Http::Headers)
+        expect(ApiGuardian.configuration).to receive(:enable_2fa).and_return(true)
+        expect(user).to receive(:otp_enabled).and_return true
+        expect(ApiGuardian.configuration).to receive(:otp_header_name).and_return('X-TEST')
+        expect(mock_request).to receive(:headers).and_return(mock_headers)
+        expect(mock_headers).to receive(:[]).with('X-TEST').and_return('')
+
+        expect(ApiGuardian::Jobs::SendOtp).to receive(:perform_later).with(user)
+
+        expect do
+          ApiGuardian::Strategies::TwoFactorAuthentication.authenticate_request(user, mock_request)
+        end.to raise_error ApiGuardian::Errors::TwoFactorRequired
+      end
+
+      it 'returns the value of authenticate_otp if code present' do
+        mock_headers = instance_double(ActionDispatch::Http::Headers)
+        expect(ApiGuardian.configuration).to receive(:enable_2fa).and_return(true)
+        expect(user).to receive(:otp_enabled).and_return true
+        expect(ApiGuardian.configuration).to receive(:otp_header_name).and_return('X-TEST')
+        expect(mock_request).to receive(:headers).and_return(mock_headers)
+        expect(mock_headers).to receive(:[]).with('X-TEST').and_return('000')
+        expect(user).to receive(:authenticate_otp).with('000', drift: 30).and_return true
+
+        result = ApiGuardian::Strategies::TwoFactorAuthentication.authenticate_request(user, mock_request)
+
+        expect(result).to be true
+      end
+
+      it 'should authenticate otp header value' do
+        password = Faker::Internet.password(32)
+
+        user = create(:user, password: password, password_confirmation: password)
+
+        result = ApiGuardian::Strategies::PasswordAuthentication.authenticate user, password
+
+        expect(result).to eq user
+
+        result = ApiGuardian::Strategies::PasswordAuthentication.authenticate user, 'password'
+
+        expect(result).to eq nil
+      end
+    end
+  end
+end
