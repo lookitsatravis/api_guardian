e35c62d0bf4e89984dd748b1fdd75ad173f8d52b
Travis Vignon <travis@lookitsatravis.com>
2015-12-09 16:25:07 -0600

    Added test for application#not_found

---
+++ spec/requests/application_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'faker'
+
+describe 'ApiGuardian::ApplicationController' do
+  # Authentication and permissions are tested elsewhere
+  before(:each) { @routes = ApiGuardian::Engine.routes }
+
+  describe 'Missing round catcher' do
+    it 'renders not found' do
+      %w(GET POST PUT PATCH DELETE OPTIONS HEAD).each do |method|
+        random_route = []
+
+        rand(1..4).times do
+          random_route << Faker::Internet.slug(Faker::Lorem.words(4).join(' '), '-')
+        end
+
+        send(method.downcase, '/' + random_route.join('/'))
+
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
+end
