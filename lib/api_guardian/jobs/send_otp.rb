02455212641a2f8f16aef8d7199550842098f34f
Travis Vignon <travis@lookitsatravis.com>
2015-12-09 17:01:06 -0600

    Updated SendOtp job to handle sms, voice, and email methods

--- lib/api_guardian/jobs/send_otp.rb
+++ lib/api_guardian/jobs/send_otp.rb
@@ -5,14 +5,29 @@ module ApiGuardian
 
       def perform(user, force = false)
         return unless user.otp_enabled?
+        send_otp(user, force)
+      end
 
-        # We force the message if the user is trying to verify their phone
-        unless force
-          unless user.phone_number.present? && user.phone_number_confirmed_at.present?
-            Rails.logger.error '[ApiGuardian] User does not have a confirmed phone number! Cannot send OTP.'
-            return
-          end
+      def send_otp(user, force)
+        case user.otp_method
+        when 'sms'
+          send_sms(user, force)
+        when 'voice'
+          send_voice(user, force)
+        when 'email'
+          send_email(user)
+        when 'google_auth'
+          Rails.logger.info '[ApiGuardian] User will provide code from Google Auth app'
+        else
+          Rails.logger.error "[ApiGuardian] No valid OTP send methods for user #{user.id}!"
         end
+      end
+
+      private
+
+      def send_sms(user, force)
+        # We force the message if the user is trying to verify their phone
+        return unless force || user_can_receive_sms?(user)
 
         body = "#{user.otp_code} is your authentication code."
 
@@ -21,6 +36,34 @@ module ApiGuardian
           to: user.phone_number,
           body: body
         )
+      rescue StandardError => e
+        Rails.logger.warn "[ApiGuardian] Could not connect to Twilio! #{e.message}"
+      end
+
+      def send_voice(user, force)
+        # We force the message if the user is trying to verify their phone
+        return unless force || user_can_receive_sms?(user)
+
+        ApiGuardian.twilio_client.calls.create(
+          from: ApiGuardian.configuration.twilio_send_from,
+          to: user.phone_number,
+          url: ApiGuardian::Engine.routes.url_helpers.voice_otp_user_url(user)
+        )
+      rescue StandardError => e
+        Rails.logger.warn "[ApiGuardian] Could not connect to Twilio! #{e.message}"
+      end
+
+      def send_email(user)
+        mailer = ApiGuardian::Mailers::Mailer
+        mailer.one_time_password(user).deliver_later
+      end
+
+      def user_can_receive_sms?(user)
+        unless user.phone_number.present? && user.phone_number_confirmed_at.present?
+          Rails.logger.error '[ApiGuardian] User does not have a confirmed phone number! Cannot send OTP.'
+          return false
+        end
+        true
       end
     end
   end
--- spec/lib/jobs/send_otp_spec.rb
+++ spec/lib/jobs/send_otp_spec.rb
@@ -2,53 +2,153 @@ describe ApiGuardian::Jobs::SendOtp do
   # Methods
   describe 'methods' do
     describe '#perform' do
-      it 'does nothing if otp_enabled is false' do
+      it 'with otp_enabled disabled does nothing' do
         user = mock_model(ApiGuardian::User)
         expect(user).to receive(:otp_enabled?).and_return(false)
 
         subject.perform(user)
       end
 
-      it 'logs error if user has no phone number' do
-        user = mock_model(ApiGuardian::User)
-        expect(user).to receive(:otp_enabled?).and_return(true)
-        expect(user).to receive(:phone_number).and_return('')
-        expect(Rails.logger).to receive(:error).with '[ApiGuardian] User does not have a confirmed phone number! Cannot send OTP.'
+      context 'with otp_enabled' do
+        let(:user) { mock_model(ApiGuardian::User) }
+        let(:mock_client) { FakeSMS.new('foo', 'bar') }
+        let(:mock_voice_client) { FakeVoice.new('foo', 'bar') }
+        let(:to_number) { '18005554321' }
+        let(:from_number) { '18885551234' }
+        let(:otp_code) { '0101' }
 
-        subject.perform(user)
-      end
+        before(:each) do
+          expect(user).to receive(:otp_enabled?).and_return(true)
+          ApiGuardian.configuration.twilio_send_from = from_number
+        end
 
-      it 'logs error if user\'s number is not confirmed' do
-        example_number = '12345'
-        user = mock_model(ApiGuardian::User)
-        expect(user).to receive(:otp_enabled?).and_return(true)
-        expect(user).to receive(:phone_number).and_return(example_number)
-        expect(user).to receive(:phone_number_confirmed_at).and_return(nil)
+        context 'can send via SMS' do
+          before(:each) do
+            expect(user).to receive(:otp_method).and_return('sms')
+          end
 
-        expect(Rails.logger).to receive(:error).with '[ApiGuardian] User does not have a confirmed phone number! Cannot send OTP.'
+          it 'catches raised StandardErrors' do
+            expect(user).to receive(:otp_code).and_return(otp_code)
+            expect(ApiGuardian).to receive(:twilio_client).and_raise('oops')
+            expect(Rails.logger).to receive(:warn).with "[ApiGuardian] Could not connect to Twilio! oops"
+            subject.perform(user, true)
+          end
 
-        subject.perform(user)
-      end
+          it 'executes when forced' do
+            expect(user).to receive(:phone_number).and_return(to_number)
+            expect(user).to receive(:otp_code).and_return(otp_code)
+            expect(ApiGuardian).to receive(:twilio_client).and_return(mock_client)
+            expect(mock_client).to receive(:create).with(
+              from: from_number,
+              to: to_number,
+              body: "#{otp_code} is your authentication code."
+            )
 
-      it 'sends message via Twilio' do
-        example_number = '54321'
-        example_confirmed = DateTime.now.utc
-        user = mock_model(ApiGuardian::User)
-        ApiGuardian.configuration.twilio_send_from = '18009876543'
-        mock_client = FakeSMS.new('foo', 'bar')
+            subject.perform(user, true)
+          end
 
-        expect(user).to receive(:otp_enabled?).and_return(true)
-        expect(user).to receive(:phone_number).twice.and_return(example_number)
-        expect(user).to receive(:phone_number_confirmed_at).and_return(example_confirmed)
-        expect(user).to receive(:otp_code).and_return('0101')
-        expect(ApiGuardian).to receive(:twilio_client).and_return(mock_client)
-        expect(mock_client).to receive(:create).with(
-          from: '18009876543',
-          to: '54321',
-          body: '0101 is your authentication code.'
-        )
+          context 'executes when user can receive SMS' do
+            it 'fails if user has no phone number' do
+              expect(user).to receive(:phone_number).and_return('')
+              expect(Rails.logger).to receive(:error).with '[ApiGuardian] User does not have a confirmed phone number! Cannot send OTP.'
+              subject.perform(user)
+            end
 
-        subject.perform(user)
+            it 'fails if user\'s phone number is not confirmed' do
+              expect(user).to receive(:phone_number).and_return(to_number)
+              expect(user).to receive(:phone_number_confirmed_at).and_return(nil)
+              expect(Rails.logger).to receive(:error).with '[ApiGuardian] User does not have a confirmed phone number! Cannot send OTP.'
+              subject.perform(user)
+            end
+
+            it 'sends message via Twilio' do
+              expect(user).to receive(:phone_number).twice.and_return(to_number)
+              expect(user).to receive(:phone_number_confirmed_at).and_return(DateTime.now)
+              expect(user).to receive(:otp_code).and_return(otp_code)
+              expect(ApiGuardian).to receive(:twilio_client).and_return(mock_client)
+              expect(mock_client).to receive(:create).with(
+                from: from_number,
+                to: to_number,
+                body: "#{otp_code} is your authentication code."
+              )
+
+              subject.perform(user)
+            end
+          end
+        end
+
+        context 'can send via voice' do
+          before(:each) do
+            expect(user).to receive(:otp_method).and_return('voice')
+            ApiGuardian::Engine.routes.default_url_options[:host] = 'http://example.com'
+          end
+
+          it 'catches raised StandardErrors' do
+            expect(ApiGuardian).to receive(:twilio_client).and_raise('oops')
+            expect(Rails.logger).to receive(:warn).with "[ApiGuardian] Could not connect to Twilio! oops"
+            subject.perform(user, true)
+          end
+
+          it 'executes when forced' do
+            expect(user).to receive(:phone_number).and_return(to_number)
+            expect(ApiGuardian).to receive(:twilio_client).and_return(mock_voice_client)
+            expect(mock_voice_client).to receive(:create).with(
+              from: from_number,
+              to: to_number,
+              url: ApiGuardian::Engine.routes.url_helpers.voice_otp_user_url(user)
+            )
+
+            subject.perform(user, true)
+          end
+
+          context 'executes when user can receive voice call' do
+            it 'fails if user has no phone number' do
+              expect(user).to receive(:phone_number).and_return('')
+              expect(Rails.logger).to receive(:error).with '[ApiGuardian] User does not have a confirmed phone number! Cannot send OTP.'
+              subject.perform(user)
+            end
+
+            it 'fails if user\'s phone number is not confirmed' do
+              expect(user).to receive(:phone_number).and_return(to_number)
+              expect(user).to receive(:phone_number_confirmed_at).and_return(nil)
+              expect(Rails.logger).to receive(:error).with '[ApiGuardian] User does not have a confirmed phone number! Cannot send OTP.'
+              subject.perform(user)
+            end
+
+            it 'initiate call via Twilio' do
+              expect(user).to receive(:phone_number).twice.and_return(to_number)
+              expect(user).to receive(:phone_number_confirmed_at).and_return(DateTime.now)
+              expect(ApiGuardian).to receive(:twilio_client).and_return(mock_voice_client)
+              expect(mock_voice_client).to receive(:create).with(
+                from: from_number,
+                to: to_number,
+                url: ApiGuardian::Engine.routes.url_helpers.voice_otp_user_url(user)
+              )
+
+              subject.perform(user)
+            end
+          end
+        end
+
+        it 'can send via email' do
+          expect(user).to receive(:otp_method).and_return('email')
+          mock_delivery = instance_double(ActionMailer::MessageDelivery)
+          expect(ApiGuardian::Mailers::Mailer).to receive(:one_time_password).with(user).and_return(mock_delivery)
+          expect(mock_delivery).to receive(:deliver_later)
+          subject.perform(user)
+        end
+
+        it 'can handle google_auth' do
+          expect(user).to receive(:otp_method).and_return('google_auth')
+          expect(Rails.logger).to receive(:info).with '[ApiGuardian] User will provide code from Google Auth app'
+          subject.perform(user)
+        end
+
+        it 'logs error for all other cases' do
+          expect(user).to receive(:otp_method).and_return('blah')
+          expect(Rails.logger).to receive(:error).with "[ApiGuardian] No valid OTP send methods for user #{user.id}!"
+          subject.perform(user)
+        end
       end
     end
   end
