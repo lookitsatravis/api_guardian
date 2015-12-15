c6b4bca2261652f77cea86c6c4ae431c33e60b32
Travis Vignon <travis@lookitsatravis.com>
2015-12-15 16:31:32 -0600

    Added custom matchers for attr_*
    * attr_accessor
    * attr_reader
    * attr_writer

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -75,3 +75,58 @@ RSpec::Matchers.define :validate_with do |expected_validator|
     "do not expected to validate with #{validator}#{@options.present? ? (' with options ' + @options) : ''}"
   end
 end
+
+RSpec::Matchers.define :have_attr_accessor do |field|
+  match do |object_instance|
+    object_instance.respond_to?(field) &&
+      object_instance.respond_to?("#{field}=")
+  end
+
+  failure_message do |object_instance|
+    "expected attr_accessor for #{field} on #{object_instance}"
+  end
+
+  failure_message_when_negated do |object_instance|
+    "expected attr_accessor for #{field} not to be defined on #{object_instance}"
+  end
+
+  description do
+    "assert there is an attr_accessor of the given name on the supplied object"
+  end
+end
+
+RSpec::Matchers.define :have_attr_reader do |field|
+  match do |object_instance|
+    object_instance.respond_to?(field)
+  end
+
+  failure_message do |object_instance|
+    "expected attr_reader for #{field} on #{object_instance}"
+  end
+
+  failure_message_when_negated do |object_instance|
+    "expected attr_reader for #{field} not to be defined on #{object_instance}"
+  end
+
+  description do
+    "assert there is an attr_reader of the given name on the supplied object"
+  end
+end
+
+RSpec::Matchers.define :have_attr_writer do |field|
+  match do |object_instance|
+    object_instance.respond_to?("#{field}=")
+  end
+
+  failure_message do |object_instance|
+    "expected attr_writer for #{field} on #{object_instance}"
+  end
+
+  failure_message_when_negated do |object_instance|
+    "expected attr_writer for #{field} not to be defined on #{object_instance}"
+  end
+
+  description do
+    "assert there is an attr_writer of the given name on the supplied object"
+  end
+end
