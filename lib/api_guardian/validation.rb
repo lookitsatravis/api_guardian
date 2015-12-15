0bf50ee617dfae3d067ccfb03a30c476b01341bb
Travis Vignon <travis@lookitsatravis.com>
2015-12-15 16:35:30 -0600

    Added ValidationResult class

---
+++ lib/api_guardian/validation.rb
@@ -0,0 +1,10 @@
+module ApiGuardian
+  class ValidationResult
+    attr_reader :succeeded, :error
+
+    def initialize(succeeded = true, error = '')
+      @succeeded = succeeded
+      @error = error
+    end
+  end
+end
---
+++ spec/lib/validation_spec.rb
@@ -0,0 +1,25 @@
+describe ApiGuardian::ValidationResult do
+  it { should have_attr_reader(:succeeded) }
+  it { should have_attr_reader(:error) }
+
+  describe 'methods' do
+    describe '#initialize' do
+      it 'should set succeeded and error' do
+        subject = ApiGuardian::ValidationResult.new
+
+        expect(subject.succeeded).to eq true
+        expect(subject.error).to eq ''
+
+        subject = ApiGuardian::ValidationResult.new(false)
+
+        expect(subject.succeeded).to eq false
+        expect(subject.error).to eq ''
+
+        subject = ApiGuardian::ValidationResult.new(true, 'test')
+
+        expect(subject.succeeded).to eq true
+        expect(subject.error).to eq 'test'
+      end
+    end
+  end
+end
