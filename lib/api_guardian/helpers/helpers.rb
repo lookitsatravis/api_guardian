344b48d91c8a02c37d90a02e799e29367070c6a2
Travis Vignon <travis@lookitsatravis.com>
2015-12-16 20:57:04 -0600

    Added helpers to match email and phone from string

--- lib/api_guardian/helpers/helpers.rb
+++ lib/api_guardian/helpers/helpers.rb
@@ -2,5 +2,17 @@ require 'api_guardian/helpers/digits'
 
 module ApiGuardian
   module Helpers
+    def self.email_address?(value)
+      # It's not really possible to validate all email addresses for all
+      # carriers, so we just check for the one character they all need. Plus,
+      # we can know it's not a phone number!
+      value.include?('@')
+    end
+
+    def self.phone_number?(value)
+      # We only have the need to match phone numbers formatted
+      # by Twitter Digits which is like +18884321000
+      !!value.match(/\+{1}\d+(?!\w)/)
+    end
   end
 end
---
+++ spec/lib/helpers/helpers_spec.rb
@@ -0,0 +1,20 @@
+describe ApiGuardian::Helpers do
+  describe 'methods' do
+    describe '.email_address?' do
+      it 'should match email addresses' do
+        expect(ApiGuardian::Helpers.email_address?('test')).to eq false
+        expect(ApiGuardian::Helpers.email_address?('test.test')).to eq false
+        expect(ApiGuardian::Helpers.email_address?('test@test')).to eq true
+        expect(ApiGuardian::Helpers.email_address?('test@test.com')).to eq true
+      end
+    end
+
+    describe '.phone_number?' do
+      it 'should only match phone numbers in +NNNNN format' do
+        expect(ApiGuardian::Helpers.phone_number?('(888) 555-1234')).to eq false
+        expect(ApiGuardian::Helpers.phone_number?('8885551234')).to eq false
+        expect(ApiGuardian::Helpers.phone_number?('+8885551234')).to eq true
+      end
+    end
+  end
+end
