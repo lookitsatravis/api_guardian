6c2b50866ce061b056467d7947a8922ab0e19e6e
Travis Vignon <travis@lookitsatravis.com>
2016-01-18 11:44:04 -0600

    Fixed Rack::Cors middleware to be Rails 5 compatible

--- lib/api_guardian/engine.rb
+++ lib/api_guardian/engine.rb
@@ -17,10 +17,10 @@ module ApiGuardian
       g.helper false
     end
 
-    config.middleware.insert_before 0, 'Rack::Cors' do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
       end
     end
 
