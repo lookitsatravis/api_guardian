84f6c026a08e5595218c9bd497e5762de589dde6
Travis Vignon <travis@lookitsatravis.com>
2016-02-15 22:15:20 -0600

    Checking in secure_compare password checker

--- lib/api_guardian/strategies/authentication/authentication.rb
+++ lib/api_guardian/strategies/authentication/authentication.rb
@@ -25,6 +25,16 @@ module ApiGuardian
 
     nil
   end
+
+  # constant-time comparison algorithm to prevent timing attacks
+  def self.secure_compare(a, b)
+    return false if a.blank? || b.blank? || a.bytesize != b.bytesize
+    l = a.unpack "C#{a.bytesize}"
+
+    res = 0
+    b.each_byte { |byte| res |= byte ^ l.shift }
+    res == 0
+  end
 end
 
 require 'api_guardian/strategies/authentication/password'
