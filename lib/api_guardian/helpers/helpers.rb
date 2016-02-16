c40e55abb7f0ef0ed79906170c2a39b7c10eed8d
Travis Vignon <travis@lookitsatravis.com>
2016-02-15 21:54:17 -0600

    Fixed bug when values are missing for helper type checks

--- lib/api_guardian/helpers/helpers.rb
+++ lib/api_guardian/helpers/helpers.rb
@@ -6,13 +6,13 @@ module ApiGuardian
       # It's not really possible to validate all email addresses for all
       # carriers, so we just check for the one character they all need. Plus,
       # we can know it's not a phone number!
-      value.include?('@')
+      value && value.include?('@')
     end
 
     def self.phone_number?(value)
       # We only have the need to match phone numbers formatted
       # by Twitter Digits which is like +18884321000
-      !!value.match(/\+{1}\d+(?!\w)/)
+      value && !!value.match(/\+{1}\d+(?!\w)/)
     end
   end
 end
