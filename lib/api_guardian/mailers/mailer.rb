c876bde1d8fdda3d759909f9c35ac17537241154
Travis Vignon <travis@lookitsatravis.com>
2016-04-21 23:27:49 -0500

    Updated reset password mail to include email

--- lib/api_guardian/mailers/mailer.rb
+++ lib/api_guardian/mailers/mailer.rb
@@ -31,9 +31,9 @@ module ApiGuardian
 
       def reset_password_body(user)
         base_reset_url = ApiGuardian.configuration.client_password_reset_url
-        reset_url = "#{base_reset_url}?token=#{user.reset_password_token}"
+        reset_url = "#{base_reset_url}?token=#{user.reset_password_token}&email=#{URI.escape(user.email)}"
         '<p>Hello!</p><p>Please click the following link to reset your password.</p>' \
-        "<p><strong><a href='#{reset_url}'>#{reset_url}</a></strong></p>" \
+        "<p><strong><a href='#{reset_url}'>Reset Password Now</a></strong></p>" \
         '<p>If you did not request to reset your password, simply ignore this email' \
         ' and nothing will change.</p><p>Thanks!</p>'
       end
