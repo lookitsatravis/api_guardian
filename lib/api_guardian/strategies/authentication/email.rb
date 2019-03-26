f0444467cbccd353b68fcc8af3d310894c9019c6
Anton Visser <visseraj@gmail.com>
2019-03-26 13:19:48 -0500

    force email to use lowercase email

--- lib/api_guardian/strategies/authentication/email.rb
+++ lib/api_guardian/strategies/authentication/email.rb
@@ -5,7 +5,7 @@ module ApiGuardian
         provides_authentication_for :email
 
         def authenticate(options)
-          user = ApiGuardian.configuration.user_class.find_by(email: options[:email])
+          user = ApiGuardian.configuration.user_class.find_by(email: options[:email].downcase)
           super(user: user)
           user if user && user.try(:authenticate, options[:password])
         end
