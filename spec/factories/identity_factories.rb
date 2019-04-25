3e8d6e1b3419d3ccebf4f4625ce4e1cac73af6c4
Travis Vignon <travis@lookitsatravis.com>
2019-04-25 09:41:51 -0500

    Fixed deprecated faker reference

--- spec/factories/identity_factories.rb
+++ spec/factories/identity_factories.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :identity, class: ApiGuardian::Identity do |f|
     f.sequence(:provider) { |n| "#{Faker::Lorem.word} #{n}" }
-    f.sequence(:provider_uid) { |n| "#{Faker::Bitcoin.address} #{n}" }
+    f.sequence(:provider_uid) { |n| "#{SecureRandom.uuid} #{n}" }
     f.tokens { { token: SecureRandom.hex(32) } }
     association :user, factory: :user
   end
